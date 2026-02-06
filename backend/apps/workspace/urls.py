3a4a99ff36793ac67f6a438a87bcde84eee6f1b6
Taona Munikwa <69734394+taonaben@users.noreply.github.com>
2026-02-06 20:26:37 +0200

    feat: Implement user authentication methods in UserViewSet; add login and me actions

--- backend/apps/workspace/urls.py
+++ backend/apps/workspace/urls.py
@@ -3,10 +3,12 @@ from rest_framework.routers import DefaultRouter
 
 from .views.workspace_views import WorkspaceViewSet
 from .views.workspace_members_views import SpaceMemberViewSet
+from .views.workspace_types import WorkspaceTypeViewSet
 
 router = DefaultRouter()
-router.register(r"", WorkspaceViewSet, basename="workspace")
+router.register(r"types", WorkspaceTypeViewSet, basename="workspace-type")
 router.register(r"members", SpaceMemberViewSet, basename="workspace-member")
+router.register(r"", WorkspaceViewSet, basename="workspace")
 
 urlpatterns = [
     path("", include(router.urls)),
